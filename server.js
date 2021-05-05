a50dbea0bb71a86091a3d1d27a0d59662870bed0
roman-digaetano <roman.digaetano@gmail.com>
2021-05-05 09:16:01 -0500

    add login POST route

--- server.js
+++ server.js
@@ -1,10 +1,15 @@
 const express = require('express');
 const app = express();
 
+const bodyParser = require('body-parser');
+app.use(bodyParser.json());
+
 const mockUserData=[
 {name:'Esther'},
 {name:'Jon'}
 ]
+
+
 app.get('/users', function(req, res){
 	res.json({
 		success: true,
@@ -14,6 +19,7 @@ app.get('/users', function(req, res){
 })
 
 
+// colos : are used as variable that can be viewed in the 'params'
 app.get('/users/:id', function(req, res){
 	console.log(req.params.id)
 	res.json({
@@ -21,6 +27,28 @@ app.get('/users/:id', function(req, res){
 		message: 'got one user',
 		user: req.params.id
 	})
+})
+
+
+app.post('/login', function(req, res){
+	const username = req.body.username;
+	const password = req.body.password;
+
+	const mockUsername = "userName";
+	const mockPassword = "superSecret";
+
+	if (username==mockUsername && password==mockPassword){
+		res.json({
+			success: true,
+			message: 'password and username match!',
+			token: 'encrypted token goes here'
+		})
+	} else {
+		res.json({
+			success: false,
+			message: 'password and username do not match.'
+		})
+	}
 })
 
 
